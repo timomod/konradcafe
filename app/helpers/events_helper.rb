a4ee605f8f4217188f966ba3601675114cb906ce
Timo Modeen <timomodeen@yahoo.com>
2016-11-01 01:32:52 +0200

    Add Opening time helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -27,6 +27,39 @@ module EventsHelper
   end
 
 
+  def opening_times
+      #Weekdays   Sun-0     Mon-1    Tues-2    Wed-3  Thu-4   Fri-5   Sat-6
+      schedule = [[11,24],[11,24],[11,24],[11,24],[11,24],[11,01],[10,01]]
+      day_num = Date.today.strftime("%w").to_i
+      day_time = Date.today.strftime("%H").to_i
+
+      today = schedule[day_num]
+      return  "Open Today: #{'%.2f' % today[0]} - #{'%.2f' % today[1]}"
+  end
+
+
+
+
+
+
+
+
+
+      # #checks the closing hour in Array is Not past midnight(i.e. that it IS later than 5 am)
+      # #and checks opening times is between opening an closing time
+      # if (schedule[day_time][2] > 5) && ( (day_time >= schedule[day_num][1]) && (day_time <= schedule[day_num][2])
+      #     # Use the day number to get right Array data
+      #     today = schedule[day_num]
+      #     "Open Today: #{ '%.2f' % today[1]} - "
+      # end
+
+
+
+
+
+
+
+
 
 
 
